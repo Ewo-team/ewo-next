45e051e3119178dc72fe9c53c27c8f5bb4fc5872
Léo Maradan <3043485+Leomaradan@users.noreply.github.com>
2019-06-21 17:20:11 +0200

    add jest config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 /*
   Server Index File
 */
-import { runCommands } from '@commands/runCommands';
 import { loadDatabases } from '@engine/actions';
 import { makeStore, store } from '@engine/store';
 import { GameServer } from '@server/server';
 import { startSocket } from '@server/socket';
+import { runCommands } from '@tasks';
 import * as path from 'path';
 
 declare namespace NodeJS {
@@ -26,30 +26,6 @@ export const server = new GameServer(store);
 
 server.launch();
 
-/// generate huge database
-/*const coords: Coord[] = [];
-
-for (let x = -500; x <= 500; x++) {
-  for (let y = -500; y <= 500; y++) {
-    const mat = (x * 500 + y);
-    const exists = mat % 100 === 0;
-    if (exists) {
-      coords.push({ x, y, mat });
-    }
-  }
-}
-
-const map = {
-  "format": "mapOfArray",
-  "data": {
-    "earth": coords
-  },
-};
-
-fs.writeFileSync('./data/maps.json', JSON.stringify(map, null, 2));
-console.log(coords);*/
-
-// store.dispatch(loadDatabase());
 store.dispatch(loadDatabases());
 
 /*console.log('prepare command');
